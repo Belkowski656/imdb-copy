b37aa3f20db768d089e1a9febf8eea534dfc464f
Belkowski656 <hub.mezykprogramming@gmail.com>
2021-07-06 15:26:37 +0200

    add slider

--- js/slider.js
+++ js/slider.js
@@ -1,52 +1,117 @@
-const arrows = document.querySelectorAll('.slider__arrow');
-const sliderElements = document.querySelectorAll('.slider__element');
+const sliders = [...document.querySelectorAll('.slider')];
 
-let translateValue = 0;
+const renderSlider = (arrows, sliderElements, sliderContainer) => {
 
-arrows.forEach(arrow => {
-    arrow.addEventListener('click', () => {
-        slider(arrow.dataset.direction)
-    })
-});
+    let translateValue = 0;
 
-const slider = (direction) => {
-    const containerWidth = document.querySelector('.slider').offsetWidth;
-    const elemWidth = sliderElements[0].offsetWidth;
+    arrows.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+            slider(arrow.dataset.direction)
+        })
+    });
 
-    const maxTranslateValue = (sliderElements.length) * elemWidth - containerWidth;
+    const slider = (direction) => {
+        const containerWidth = sliderContainer.offsetWidth;
+        const elemWidth = sliderElements[0].offsetWidth + 10;
+
+        const maxTranslateValue = (sliderElements.length) * elemWidth - containerWidth;
 
 
-    if (direction === 'right') {
+        if (direction === 'right') {
 
-        if (maxTranslateValue <= translateValue) return;
+            if (maxTranslateValue <= translateValue) return;
 
-        translateValue += elemWidth;
+            translateValue += elemWidth;
 
-        showAndHideArrows(maxTranslateValue);
-        sliding(translateValue);
-    } else {
-        if (0 === translateValue) return;
+            showAndHideArrows(maxTranslateValue);
+            sliding(translateValue);
+        } else {
+            if (0 === translateValue) return;
 
-        translateValue -= elemWidth;
+            translateValue -= elemWidth;
 
-        showAndHideArrows(maxTranslateValue);
-        sliding(translateValue);
+            showAndHideArrows(maxTranslateValue);
+            sliding(translateValue);
+        }
+
     }
 
-}
+    const sliding = () => {
+        sliderElements.forEach(elem => {
+            elem.style.transform = `translateX(-${translateValue}px)`;
+        });
+    }
 
-const sliding = () => {
-    sliderElements.forEach(elem => {
-        elem.style.transform = `translateX(-${translateValue}px)`;
-    });
-}
+    const showAndHideArrows = (maxTranslateValue) => {
+        if (translateValue === 0) arrows[0].style.display = 'none';
+        else arrows[0].style.display = 'block';
 
-const showAndHideArrows = (maxTranslateValue) => {
-    if (translateValue === 0) arrows[0].style.display = 'none';
-    else arrows[0].style.display = 'block';
+        if (maxTranslateValue <= translateValue) arrows[1].style.display = 'none';
+        else arrows[1].style.display = 'block';
+    };
 
-    if (maxTranslateValue <= translateValue) arrows[1].style.display = 'none';
-    else arrows[1].style.display = 'block';
+    showAndHideArrows();
 };
 
-showAndHideArrows();
+sliders.forEach(slider => {
+    const childrens = [...slider.children];
+    const arrows = [...childrens.filter(children => children.classList.value.includes('slider__arrow'))];
+    const sliderElements = [...childrens.filter(children => children.classList.value.includes('slider__element'))];
+    console.log(childrens, arrows, sliderElements);
+    renderSlider(arrows, sliderElements, slider);
+});
+
+// const renderSlider = (arrows, sliderElements, slider) => {
+//     const arrows = document.querySelectorAll('.slider__arrow');
+//     const sliderElements = document.querySelectorAll('.slider__element');
+
+//     let translateValue = 0;
+
+//     arrows.forEach(arrow => {
+//         arrow.addEventListener('click', () => {
+//             slider(arrow.dataset.direction)
+//         })
+//     });
+
+//     const slider = (direction) => {
+//         const containerWidth = document.querySelector('.slider').offsetWidth;
+//         const elemWidth = sliderElements[0].offsetWidth + 10;
+
+//         const maxTranslateValue = (sliderElements.length) * elemWidth - containerWidth;
+
+
+//         if (direction === 'right') {
+
+//             if (maxTranslateValue <= translateValue) return;
+
+//             translateValue += elemWidth;
+
+//             showAndHideArrows(maxTranslateValue);
+//             sliding(translateValue);
+//         } else {
+//             if (0 === translateValue) return;
+
+//             translateValue -= elemWidth;
+
+//             showAndHideArrows(maxTranslateValue);
+//             sliding(translateValue);
+//         }
+
+//     }
+
+//     const sliding = () => {
+//         sliderElements.forEach(elem => {
+//             elem.style.transform = `translateX(-${translateValue}px)`;
+//         });
+//     }
+
+//     const showAndHideArrows = (maxTranslateValue) => {
+//         if (translateValue === 0) arrows[0].style.display = 'none';
+//         else arrows[0].style.display = 'block';
+
+//         if (maxTranslateValue <= translateValue) arrows[1].style.display = 'none';
+//         else arrows[1].style.display = 'block';
+//     };
+
+//     showAndHideArrows();
+// };
